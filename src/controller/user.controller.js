f6347a9f2cf7e4faafc3d9b419dd0ff542fd2a6a
Rodrigo Faria <rodrigolfsi@gmail.com>
2024-01-20 07:38:13 -0300

    Add user validation payload

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -6,6 +6,19 @@ const create = (req, res) => {
     res.status(200).send('User create successfully')
 }
 
+const isValidUser = (req, res, next) => {
+    const user = req.body
+    if (!user.name || user.name.trim() === '')
+        res.status(400).send('Required field "name" is missing')
+    else if (!user.email || user.email.trim() === '')
+        res.status(400).send('Required field "email" is missing')
+    else if (!user.password || user.password.trim() === '')
+        res.status(400).send('Required field "password" is missing')
+    else
+        next()
+}
+
 module.exports = {
-    create
+    create,
+    isValidUser
 }
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controller/user.controller')
 
-router.post('/', controller.create)
+router.post('/', controller.isValidUser, controller.create)
 
 module.exports = router
